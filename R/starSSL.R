c81899979202c0687bf13743bbe22823ac3c998e
YunyiShen <yshen99@wisc.edu>
2021-09-20 10:03:59 -0500

    more flexibility to the link function in star

--- R/starSSL.R
+++ R/starSSL.R
@@ -1,8 +1,23 @@
-#' Multivariate STAR Spike-and-Slab LASSO with fixed link function and floor rouding
+
+#' the logfloor transformation to integer values
+#' calculate the latent normal's bound under log-floor ransformation, i.e. y=floor(exp(z*)), z*~N
+#' @param Y the observed data
+#' @param upper whether to calculate upper bound
+#' @return a matrix of lower or upper bound of the latent normal
+logfloorlink <- function(Y, upper = TRUE){
+    if(upper){
+        return(log(Y+1))
+    }
+    return(log(Y))
+}
+
+
+
+#' Multivariate STAR Spike-and-Slab LASSO with fixed link function
 #' @description Main posterior exploration algorithm for multivariate STAR spike-and-slab LASSO with fixed link function and floor rouding.
 #' @param Y response matrix
 #' @param X design matrix
-#' @param link the known link function for STAR model
+#' @param link the function calculate the bounds of the latent normal h^-1(g(y*)), default is floor rounding with log link, should be a function with two arguments, the data and a boolean argument, if true we calculate the upper bound and lower bound otherwise.
 #' @param condexp bool, whether to do the fast conditional posterior exploration (dcpe), default is FALSE for doing the dynamic posterior exploration (dpe)
 #' @param lambdas hyperparameters to be explored by the algorithm, penalty on B
 #' @param xis hyperparameters to be explored by the algorithm, penalty on Omega
@@ -16,7 +31,7 @@
 #' @param verbose bool, whether to print intermidate notes
 #' @return A list with dynamic exploration result, point estimates are in `$Omega` and `$B`.
 #' 
-fstarmSSL <- function(Y,X,link = log,condexp = FALSE, 
+fstarmSSL <- function(Y,X,link = logfloorlink,condexp = FALSE, 
                     lambdas = list(lambda1 = 1, lambda0 = seq(10, nrow(X), length = 10)),
                   xis = list(xi1 = 0.01 * nrow(X), xi0 = seq(0.1 * nrow(X), nrow(X), length = 10)),
                   theta_hyper_params = c(1, ncol(X) * ncol(Y)),
@@ -36,8 +51,8 @@ fstarmSSL <- function(Y,X,link = log,condexp = FALSE,
         stop("X and Y should have the number of rows.")
     }
 
-    lower <- link(Y)
-    upper <- link(Y + 1)
+    upper <- link(Y,TRUE)
+    lower <- link(Y,FALSE)
 
     # mean-cov parameterization 
     if(condexp){
