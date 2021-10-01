39b14450e218cae10707aa725b22af2314d62297
YunyiShen <yshen99@wisc.edu>
2021-09-30 20:33:02 -0500

    fake base R graph

--- tests/cgSSL_Formal_simu/p10q10n100/plot_B_learning.R
+++ tests/cgSSL_Formal_simu/p10q10n100/plot_B_learning.R
@@ -16,59 +16,91 @@ B_learning_q10[is.na(B_learning_q10)] <- 0
 #B_learning_q10$beta.sparsity <- B_learning_q10$s
 
 ## New plot
-B_learning_q10 <- within(B_learning_q10, algo<-factor(algo, levels=c("cgSSL-dpe", "cgSSL-dcpe", "mSSL-dpe","mSSL-dcpe", "CAR-A", "CAR" )))
+B_learning_q10 <- within(B_learning_q10, algo<-factor(algo, levels= rev( c("cgSSL-dpe", "cgSSL-dcpe", "mSSL-dpe","mSSL-dcpe", "CAR-A", "CAR" ))))
 B_MCC <- ggplot(data = B_learning_q10,aes(x=algo,y = MCC)) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
   ylab("MCC on B") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
-B_MCC
-ggsave("./B_MCC.jpg",B_MCC,width = 8,height = 5,unit = "in")
+B_MCC 
+ggsave("./B_MCC.jpg",B_MCC,width = 10,height = 5,unit = "in")
 
 
-B_Sensitivity <- ggplot(data = B_learning_q10[B_learning_q10$mod!="model 5",],aes(x=algo,y = SEN)) + 
+B_Sensitivity <-  ggplot(data = B_learning_q10,aes(x=algo,y = SEN)) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
   ylab("Sensitivity on B") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
 B_Sensitivity
 
 
-B_Specificity <- ggplot(data = B_learning_q10[B_learning_q10$mod!="model 5",],aes(x=algo,y = SPE)) + 
+B_Specificity <-  ggplot(data = B_learning_q10,aes(x=algo,y = SPE)) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
   ylab("Specificity on B") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
 B_Specificity
 
 
-B_FROB <- ggplot(data = B_learning_q10,aes(x=algo,y = log(FROB))) + 
+B_FROB <-  ggplot(data = B_learning_q10,aes(x=algo,y = log(FROB))) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
-  ylab("log FROB on B") + 
+  ylab("log FROB loss on B") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
 B_FROB
--- tests/cgSSL_Formal_simu/p10q10n100/plot_graph_learning.R
+++ tests/cgSSL_Formal_simu/p10q10n100/plot_graph_learning.R
@@ -14,59 +14,91 @@ Omega_learning_p10[is.na(Omega_learning_p10)] <- 0
 #Omega_learning_p10$beta.sparsity <- Omega_learning_p10$s
 
 ## New plot
-Omega_learning_p10 <- within(Omega_learning_p10, algo<-factor(algo, levels=c("cgSSL-dpe", "cgSSL-dcpe", "mSSL-dpe","mSSL-dcpe", "CAR-A", "CAR" )))
-Graph_MCC <- ggplot(data = Omega_learning_p10[Omega_learning_p10$mod!="Full",],aes(x=algo,y = MCC)) + 
+Omega_learning_p10 <- within(Omega_learning_p10, algo<-factor(algo, levels=rev(c("cgSSL-dpe", "cgSSL-dcpe", "mSSL-dpe","mSSL-dcpe", "CAR-A", "CAR" ))))
+Graph_MCC <-  ggplot(data = Omega_learning_p10[Omega_learning_p10$mod!="Full",],aes(x=algo,y = MCC)) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
   ylab("MCC on Omega") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
 Graph_MCC
-ggsave("Omega_MCC.jpg",Graph_MCC,width = 8,height = 5,unit = "in")
+ggsave("Omega_MCC.jpg",Graph_MCC,width = 9,height = 5,unit = "in")
 
 
 Graph_Sensitivity <- ggplot(data = Omega_learning_p10[Omega_learning_p10$mod!="Full",],aes(x=algo,y = SEN)) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
   ylab("Sensitivity on Omega") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
 Graph_Sensitivity
 
 
 Graph_Specificity <- ggplot(data = Omega_learning_p10[Omega_learning_p10$mod!="Full",],aes(x=algo,y = SPE)) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
   ylab("Specificity on Omega") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
 Graph_Specificity
 
 
 Graph_FROB <- ggplot(data = Omega_learning_p10[Omega_learning_p10$mod!="Full",],aes(x=algo,y = log(FROB))) + 
   geom_point( alpha=0.1, size=1)+
-  geom_boxplot() + 
+  geom_boxplot(linetype = "dashed", outlier.shape = 1) + 
+  stat_boxplot(aes(ymin = ..lower.., ymax = ..upper..), outlier.shape = 1) +
+  stat_boxplot(geom = "errorbar", aes(ymin = ..ymax..), width = 0.5)+
+  stat_boxplot(geom = "errorbar", aes(ymax = ..ymin..), width = 0.5)+
   facet_grid(~mod) + 
-  ylab("log FROB on Omega") + 
+  ylab("log FROB loss on Omega") + 
   xlab("") + theme_bw() +
   theme(legend.position="top") + 
   theme(text = element_text(size=14), 
-        axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
-        plot.margin = margin(.15, .15, .15, .15, "cm"))
+        #axis.text.x = element_text(angle=90,hjust = 1,vjust=0.5),
+        axis.text.x = element_text(angle=45,hjust = 1,vjust=1),
+        #axis.text.y = element_text(angle=180),
+        plot.margin = margin(.15, .15, .15, .15, "cm")) + 
+  theme(panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) + coord_flip()
 
 Graph_FROB
