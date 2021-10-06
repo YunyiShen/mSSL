45d87e8c495b15e4a573c6d93519f8d3f157a81a
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2021-10-05 19:15:57 -0500

    some results

--- real_data/GeneNet_timeseries/analysis.R
+++ real_data/GeneNet_timeseries/analysis.R
@@ -1,5 +1,6 @@
 library(mSSL)
 nine_genes <- read.csv("./real_data/GeneNet_timeseries/nine_gene.csv", row.names = 1)
-nine_genes <- apply(nine_genes, 2, function(w){w-mean(w)})[1:6,]
+nine_genes <- rbind(nine_genes[1:6*3-2,] ,  nine_genes[1:6*3-1,] ,  nine_genes[1:6*3,])
+nine_genes <- apply(nine_genes, 2, function(w){w-mean(w)})
 dpe_res <- mSSL::cgVARSSL(nine_genes,verbose = T, s_max_condition = 5000)
 save.image("./real_data/GeneNet_timeseries/cgVARres.RData")
